3259cc2b80f509a5f3b60b50bea5b97b901725b1
varrockstar <gregwestover11@gmail.com>
2024-12-18 20:12:49 -0500

    add part 2

--- second.py
+++ second.py
@@ -1,9 +1,11 @@
+
 def safe_rows(filename):
     with open (filename, 'r') as file:
         rows = [list(map(int, line.split())) for line in file]
     
     
     saferows = 0
+    unsaferows = []
     for row in rows:
         safecheck = 0
         for i in range(len(row)):
@@ -17,10 +19,45 @@ def safe_rows(filename):
                 pass
         if abs(safecheck) == (len(row)-1):
             saferows += 1
+        else: 
+            unsaferows += [row]
+    return unsaferows
+
+def p2_safe(list_unsafe):
+    saferows = 0
+    for row in list_unsafe:
+        loopcheck = 0
+        for i in range(len(row)):
+            x = row.pop(i)
+            safecheck = 0
+            for n in range(len(row)):
+                if n == (len(row) - 1):
+                    pass
+                elif row[n] - row[n+1] <= 3 and row[n] - row[n+1] > 0:
+                    safecheck += 1
+                elif row[n] - row[n+1] >= -3 and row[n] - row[n+1] < 0:
+                    safecheck -= 1
+                else:
+                    pass
+            
+            if abs(safecheck) == len(row) - 1:
+                loopcheck += 1
+            row.insert(i, x)
+        if loopcheck != 0:
+            saferows += 1
+                    
+            
+            
     print(saferows)
 
+
+
+
+
+
 def main():
-    safe_rows('day2_input.txt')
+    p2_safe(safe_rows('day2_input.txt'))
+
 
 if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-x = '123'
-y = int(x)
-print(y)
+list1 = [5, 6, 7, 8, 9]
+for i in list1:
+    index = list1.index(i)
+    x = list1.pop(index)
+    list1.insert(index, x)
+
